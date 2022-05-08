78e48a25e382c4dae696b56df39069c0ea2aabe6
majid <majidvayeghani@gmail.com>
2022-05-08 19:55:47 +0430

    create Task table. the db still out of the stage(git)

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+
+class Task(models.models):
+    """docstring for Task."""
+
+    _id = models.IntegerField()
+    group_id = models.IntegerField()
+    tag_id = models.IntegerField()
+    name =models. CharField(max_length = 255)
+    description = models.CharField(max_length = 500)
+    duedate = models.DataField()
+    priority_list = (
+    ( L , 'Low'),
+    ( M , 'Middel'),
+    ( H , 'High'),
+    )
+    pariority = models.CharField (
+        max_length = 1,
+        choices = pariority_list,
+        default =  M,
+        )
+    completed_at = models.DataField()
+    Created_at =  models.DataField()
+    updated_at =  models.DataField()
+    deleted_at =  models.DataField()
