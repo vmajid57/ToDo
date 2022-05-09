4d23fe526b4b46be3af5d7f71d1ce13ff10091bd
majid <majidvayeghani@gmail.com>
2022-05-09 18:50:51 +0430

    create tables with primary_key and some edit on models.py

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Task(models.models):
+class Task(models.Model):
     """docstring for Task."""
 
-    _id = models.IntegerField()
+    _id = models.IntegerField(primary_key=True)
     group_id = models.IntegerField()
     tag_id = models.IntegerField()
-    name =models. CharField(max_length = 255)
+    name = models. CharField(max_length = 255)
     description = models.CharField(max_length = 500)
     duedate = models.DataField()
     priority_list = (
@@ -23,3 +23,27 @@ class Task(models.models):
     Created_at =  models.DataField()
     updated_at =  models.DataField()
     deleted_at =  models.DataField()
+
+class Group(models.Model):
+    """docstring for Group."""
+    _id = models.IntegerField(primary_key=True)
+    name =models. CharField(max_length = 255)
+    description = models.CharField(max_length = 500)
+    priority_list = (
+    ( L , 'Low'),
+    ( M , 'Middel'),
+    ( H , 'High'),
+    )
+    pariority = models.CharField (
+        max_length = 1,
+        choices = pariority_list,
+        default =  M,
+        )
+    Created_at =  models.DataField()
+    updated_at =  models.DataField()
+    deleted_at =  models.DataField()
+
+class Tag(models.Model):
+    """docstring for Tag."""
+    _id = models.IntegerField(primary_key=True)
+    name =models. CharField(max_length = 255)
